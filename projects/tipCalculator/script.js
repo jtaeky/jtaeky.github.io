7abf910997933d939dc303aa1c04989641a814d0
John Kim <johntaekyun@Johns-MacBook-Air.local>
2026-01-06 23:45:07 +0900

    Updated styling for website and redid tip calculator

--- projects/tipCalculator/script.js
+++ projects/tipCalculator/script.js
@@ -1,73 +1,77 @@
-const billWrap = document.querySelector(".bill-input")
-const tipWrap = document.querySelector(".tip-percentage-input")
 
-const billCap = document.querySelector(".bill-caption")
-const tipCap = document.querySelector(".tip-caption")
-const finCap = document.querySelector(".final-caption")
 
-const bill = document.getElementById("bill")
-const tipPercent = document.getElementById("tip-percent")
-const tipDisplay = document.getElementById("tip-display")
 
-const reset = document.getElementById("reset")
 
-let billVal = ""
-let tipVal = ""
+// const billWrap = document.querySelector(".bill-input")
+// const tipWrap = document.querySelector(".tip-percentage-input")
 
-const STATE = {
-    BILL: "bill",
-    TIP: "tip",
-    RESULT: "result"
-}
+// const billCap = document.querySelector(".bill-caption")
+// const tipCap = document.querySelector(".tip-caption")
+// const finCap = document.querySelector(".final-caption")
 
-const stateConfig = {
-    bill: {
-        show: [billWrap, billCap],
-        hide: [tipWrap, tipCap, finCap, tipDisplay]
-    },
-    tip: {
-        show: [tipWrap, tipCap],
-        hide: [billWrap, billCap, finCap, tipDisplay]
-    },
-    result: {
-        show: [finCap, tipDisplay],
-        hide: [billWrap, billCap, tipWrap, tipCap]
-    }
-}
+// const bill = document.getElementById("bill")
+// const tipPercent = document.getElementById("tip-percent")
+// const tipDisplay = document.getElementById("tip-display")
 
-function onOff(state){
-    Object.values(stateConfig).forEach(({show, hide}) => {
-        show.forEach(el=>el.classList.add("hidden"))
-        hide.forEach(el=>el.classList.add("hidden"))
-        })   
+// const reset = document.getElementById("reset")
 
-    stateConfig[state].show.forEach(el=>el.classList.remove("hidden"))
-}
+// let billVal = ""
+// let tipVal = ""
 
-bill.addEventListener("keydown", (e)=>{
-    const val = Number(bill.value)
-    if(e.key === "Enter" && !Number.isNaN(val) && val>0){
-        billVal = val
-        onOff(STATE.TIP)
-        bill.value = ""
-        }
-})
+// const STATE = {
+//     BILL: "bill",
+//     TIP: "tip",
+//     RESULT: "result"
+// }
 
-tipPercent.addEventListener("keydown", (e)=>{
-    const val = Number(tipPercent.value)
-    if(e.key === "Enter" && !Number.isNaN(val) && val>0 && val<=100){
-        tipVal = val
-        onOff(STATE.RESULT)
-        tipPercent.value = ""
-        tipDisplay.textContent = calcTip()
-        }
-})
+// const stateConfig = {
+//     bill: {
+//         show: [billWrap, billCap],
+//         hide: [tipWrap, tipCap, finCap, tipDisplay]
+//     },
+//     tip: {
+//         show: [tipWrap, tipCap],
+//         hide: [billWrap, billCap, finCap, tipDisplay]
+//     },
+//     result: {
+//         show: [finCap, tipDisplay],
+//         hide: [billWrap, billCap, tipWrap, tipCap]
+//     }
+// }
 
-reset.addEventListener("click", ()=>{
-    console.log("fire")
-        onOff(STATE.BILL)
-})
+// function onOff(state){
+//     Object.values(stateConfig).forEach(({show, hide}) => {
+//         show.forEach(el=>el.classList.add("hidden"))
+//         hide.forEach(el=>el.classList.add("hidden"))
+//         })   
 
-function calcTip(){
-    return `$${(billVal * (tipVal/100)).toFixed(2)}`
-}
+//     stateConfig[state].show.forEach(el=>el.classList.remove("hidden"))
+// }
+
+// bill.addEventListener("keydown", (e)=>{
+//     const val = Number(bill.value)
+//     if(e.key === "Enter" && !Number.isNaN(val) && val>0){
+//         billVal = val
+//         onOff(STATE.TIP)
+//         bill.value = ""
+//         }
+// })
+
+// tipPercent.addEventListener("keydown", (e)=>{
+//     const val = Number(tipPercent.value)
+//     if(e.key === "Enter" && !Number.isNaN(val) && val>0 && val<=100){
+//         tipVal = val
+//         onOff(STATE.RESULT)
+//         tipPercent.value = ""
+//         tipDisplay.textContent = calcTip()
+//         }
+// })
+
+// reset.addEventListener("click", ()=>{
+//     console.log("fire")
+//         onOff(STATE.BILL)
+// })
+
+// function calcTip(){
+//     return `$${(billVal * (tipVal/100)).toFixed(2)}`
+// }
